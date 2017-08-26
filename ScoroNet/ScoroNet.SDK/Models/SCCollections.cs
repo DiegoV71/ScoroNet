ff7462f6e4fa0152c1e88d0778627d15aa88b21a
liptondev <zam.kuratora@gmail.com>
2017-08-26 13:40:52 +0300

    added collections

---
+++ ScoroNet/ScoroNet.SDK/Models/SCCollections.cs
@@ -0,0 +1,9 @@
+﻿namespace ScoroNet.SDK.Models
+{
+    public class SCCollections
+    {
+        public SCCollectionItem Devices { get; set; }
+        public SCCollectionItem Roles { get; set; }
+        public SCCollectionItem Users { get; set; }
+    }
+}
---
+++ ScoroNet/ScoroNet.SDK/Models/SCField.cs
@@ -0,0 +1,12 @@
+﻿namespace ScoroNet.SDK.Models
+{
+    public class SCField
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Target { get; set; }
+        public bool System { get; set; }
+        public bool ReadOnly { get; set; }
+        public bool Required { get; set; }
+    }
+}
